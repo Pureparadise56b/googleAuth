6375c56c25239efdc19574f9211be764079d2402
PureParadise56b <toufique26ajay@gmail.com>
2024-03-09 23:41:48 +0530

    test

--- src/db/connection.db.js
+++ src/db/connection.db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from '../constants.js'
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
 
 const connectDb = async () => {
   try {
     const response = await mongoose.connect(
       `${process.env.MONGO_URI}/${DB_NAME}`
     )
-    console.log('\nMongodb Connected...')
-    console.log(`Connection Host: ${response.connection.host}`)
+    console.log("\nMongodb Connected...")
+    console.log(`Connection Hos: ${response.connection.host}`)
   } catch (error) {
     console.error(error)
   }
